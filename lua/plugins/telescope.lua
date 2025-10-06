16f65226d4d201776500c81f613a95f9d3a15b77
jackycamp <jackcampanella@protonmail.com>
2025-10-06 07:12:31 -0400

    adjust telescope keybinds

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,12 +11,12 @@ return {
   },
   keys = {
     -- Find
-    { "<leader>ff", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
-    { "<leader>fF", LazyVim.pick("files"), desc = "Find Files (root)" },
+    { "<leader>fF", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
+    { "<leader>ff", LazyVim.pick("files"), desc = "Find Files (root)" },
 
     -- Search
-    { "<leader>sg", LazyVim.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
-    { "<leader>sG", LazyVim.pick("live_grep"), desc = "Grep (root)" },
+    { "<leader>sG", LazyVim.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
+    { "<leader>sg", LazyVim.pick("live_grep"), desc = "Grep (root)" },
     -- {
     --   "<leader>sG",
     --   function()
